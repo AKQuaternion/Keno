c09e2da417993c95ed23e1961d0de41e09886278
Chris Hartman <chris.m.hartman@gmail.com>
2015-04-05 11:29:31 -0800

    Initial Commit

---
+++ Keno2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Keno2
+//
+//  Created by Chris Hartman on 4/5/15.
+//  Copyright (c) 2015 Chris Hartman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
